5705a909900252e9731015463979abb81dfaaad4
yanggak12 <yanggak12@naver.com>
2022-06-14 22:16:09 +0900

    feat : geometry

---
+++ geometry/index.js
@@ -0,0 +1,97 @@
+import * as THREE from "../node_modules/three/build/three.module.js";
+import { OrbitControls } from "../node_modules/three/examples/jsm/controls/OrbitControls.js";
+
+class App {
+  constructor() {
+    const divContainer = document.querySelector("#container");
+    this._divContainer = divContainer;
+
+    const renderer = new THREE.WebGL1Renderer({ alpha: true, antialias: true });
+    renderer.setPixelRatio(window.devicePixelRatio);
+    divContainer.appendChild(renderer.domElement);
+
+    this._renderer = renderer;
+
+    const scene = new THREE.Scene();
+    this._scene = scene;
+
+    this._setupCamera();
+    this._setupLight();
+    this._setupModel();
+    this._setupControls();
+
+    window.onresize = this.resize.bind(this);
+    this.resize();
+
+    requestAnimationFrame(this.render.bind(this));
+  }
+
+  _setupControls() {
+    new OrbitControls(this._camera, this._divContainer);
+  }
+
+  _setupCamera() {
+    const width = this._divContainer.clientWidth;
+    const height = this._divContainer.clientHeight;
+    const camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 100);
+    camera.position.z = 3;
+    this._camera = camera;
+  }
+
+  _setupLight() {
+    const color = 0xffffff;
+    const intensity = 1;
+    const light = new THREE.DirectionalLight(color, intensity);
+    light.position.set(-1, 2, 4);
+    this._scene.add(light);
+  }
+
+  _setupModel() {
+    const geometry = new THREE.TorusKnotGeometry(0.6, 0.1, 64, 32, 3, 8);
+    const fillMaterial = new THREE.MeshPhongMaterial({ color: 0x515151 });
+    const cube = new THREE.Mesh(geometry, fillMaterial);
+
+    const lineMaterial = new THREE.LineBasicMaterial({ color: 0xffff00 });
+    const line = new THREE.LineSegments(
+      new THREE.WireframeGeometry(geometry),
+      lineMaterial
+    );
+
+    const group = new THREE.Group();
+    group.add(cube);
+    group.add(line);
+
+    this._scene.add(group);
+    this._cube = group;
+  }
+
+  resize() {
+    const width = this._divContainer.clientWidth;
+    const height = this._divContainer.clientHeight;
+
+    this._camera.aspect = width / height;
+    this._camera.updateProjectionMatrix();
+
+    this._renderer.setSize(width, height);
+  }
+
+  render(time) {
+    this._renderer.render(this._scene, this._camera);
+    this.update(time);
+    requestAnimationFrame(this.render.bind(this));
+  }
+
+  update(time) {
+    time *= 0.001; // second unit
+
+    // 자동으로 회전
+    // this._cube.rotation.x = time;
+    // this._cube.rotation.y = time;
+
+    // 마우스로 회전
+  }
+}
+
+window.onload = function () {
+  new App();
+};
